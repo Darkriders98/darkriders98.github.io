ef7132f09748f9662524a43038d3424da14e2cd9
Alexis <alexis.mathoux@gmail.com>
2019-03-23 15:48:36 +0100

    end JS

--- js/script.js
+++ js/script.js
@@ -9,12 +9,21 @@ function init() {
     let images = document.createElement("img");
     document.getElementById("new_element").appendChild(images).src = "icons/flower2.png";
 
-    let spans = document.getElementById("rainbow");
-    let colors = ["red", "orange", "yellow", "green", "blue", "purple", "pink"];
+    let spans = document.getElementById("rainbow").getElementsByTagName("span");
+    let colors = [ "red", "orange", "yellow", "green", "blue", "purple", "pink"];
 
     for (let i = spans.length - 1; i >= 0; i--) {
         spans[i].style.color = colors[i];
     }
 
+    let changeSrc = function(event) {
+        let filename = event.target.src.replace(/^.*[\\\/]/, '');
+        if (filename === "flower1.png") {
+            event.target.src = "icons/flower2.png";
+        }
+        else event.target.src = "icons/flower1.png";
+    };
+
+    document.getElementById("event").addEventListener("mouseover", changeSrc);
 
 }

5e6cab973df8bdf9d57c96185affd95b885d570a
Alexis <alexis.mathoux@gmail.com>
2019-04-07 14:45:07 +0200

    Restructuration

--- friendbook.php
+++ friendbook.php
@@ -5,7 +5,7 @@
     <title>
         My friend's book
     </title>
-    <link href="../css/styles.css" rel="stylesheet">
+    <link href="css/styles.css" rel="stylesheet">
 </head>
 <body>
 
@@ -14,9 +14,9 @@
 </header>
 
 <ul id="navbar">
-    <li><a href="../index.html">Home</a></li>
-    <li><a href="../tasks.html">Tasks</a></li>
-    <li><a href="../shopping.html">Shopping</a></li>
+    <li><a href="index.html">Home</a></li>
+    <li><a href="tasks.html">Tasks</a></li>
+    <li><a href="shopping.html">Shopping</a></li>
     <li><a href="friendbook.php">Friend's book</a></li>
     <li><a href="https://www.blizzard.com/en-us/">Blizzard website</a></li>
 </ul>
@@ -29,7 +29,6 @@
 <?php
 $filename = 'friends.txt';
 $file = fopen($filename, "a");
-
 
 if (array_key_exists('name', $_POST)) {
     fwrite($file, $_POST['name'] . "\n");
@@ -46,16 +45,35 @@ fclose($file);
 $filename = 'friends.txt';
 $file = fopen($filename, "r");
 
+$nameFilter = "";
+
+if (array_key_exists('nameFilter', $_POST)){
+    $nameFilter = $_POST['nameFilter'];
+}
+
 echo "<ul>";
 while (!feof($file)) {
     $names = fgets($file);
-    if ($names != "") {
-        echo "<li>$names</li><br>";
+    if ($nameFilter == "") {
+        if ($names != "") {
+            echo "<li>$names</li><br>";
+        }
+    }
+    else{
+        if ($names == $nameFilter){
+            echo "<li>$names</li><br>";
+        }
     }
 }
 echo "</ul>";
 fclose($file);
 ?>
+
+<form action="friendbook.php" method="post">
+    <input type="text" name="nameFilter" value="<?=$nameFilter?>" placeholder="Name filter">
+    <input type="submit" style="border-radius: 20%; background-color: darkgoldenrod">
+</form>
+
 
 </body>
 </html>
